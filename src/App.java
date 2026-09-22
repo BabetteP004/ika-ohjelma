59ebc0087052f189ad26c38086519db9766d374a
BabetteP004 <babettepongo7@gmail.com>
2026-09-22 13:00:41 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+ int ika = 20;
+ // Tulostusehdot
+ if (ika > 0 && ika < 18)
+{
+      System.out.println("Olet alaikainen");
+ }
+    else if (ika >= 65)
+ {
+      System.our.println("Olet elakelainen");
+ }
+    else
+{
+      System.out.println("Olet aikuinen");
+
+}
+    
+
+    }
+}
